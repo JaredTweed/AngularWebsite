94707061e716d9c221fe52c1bc358e86501abd1a
JaredTweedSFU <jaredtwe@gmail.com>
2024-04-05 15:34:32 -0700

    make dark background with spinning blurry color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nct';
+
+  blobX = 0;
+  blobY = 0;
+
+  mouseX = 0;
+  mouseY = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+  updateMousePosition(event: MouseEvent): void {
+    this.mouseX = event.pageX;
+    this.mouseY = event.pageY;
+  }
+
+
+  updatePosition(): void {
+
+    const speed = 0.01;
+    const targetX = this.mouseX;
+    const targetY = this.mouseY;
+
+
+    setTimeout(() => {
+      this.blobX += (targetX - this.blobX) * speed;
+      this.blobY += (targetY - this.blobY) * speed;
+
+    }, 200);
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {
+
+      if (this.blobX !== this.mouseX && this.blobY != this.mouseY) {
+        this.updatePosition();
+      }
+    }, 1);
+  }
 }
